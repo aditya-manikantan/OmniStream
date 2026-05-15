a7be7cc06d1e28b3d420c20603e22aa8473c98c2
Aditya Manikantan <adityamanikantan98@gmail.com>
2026-05-15 18:25:02 -0400

    Initial client app setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+
+  return (
+    <div style={{fontSize: '1.6rem'}}>OmniStream</div>
+  )
+}
+
+export default App
